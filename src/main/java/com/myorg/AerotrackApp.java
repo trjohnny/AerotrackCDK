b7747c4f1f048f88d2dba13fca51d3ba83024ea4
thoma <rossimelthomas@gmail.com>
2023-11-15 12:13:17 +0100

    feat: add pipelien stages

--- src/main/java/com/myorg/AerotrackApp.java
+++ src/main/java/com/myorg/AerotrackApp.java
@@ -4,19 +4,11 @@ import software.amazon.awscdk.App;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.StackProps;
 
-import java.util.Arrays;
-
 public class AerotrackApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new AerotrackStack(app, "AerotrackStack", StackProps.builder()
-                /*
-                .env(Environment.builder()
-                        .account(System.getenv("CDK_DEFAULT_ACCOUNT"))
-                        .region(System.getenv("CDK_DEFAULT_REGION"))
-                        .build())
-                */
+        new PipelineStack(app, "PipelineStack", StackProps.builder()
                 .env(Environment.builder()
                         .account("073873382417")
                         .region("eu-west-1")
