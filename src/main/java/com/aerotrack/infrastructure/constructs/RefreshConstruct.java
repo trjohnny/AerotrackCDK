1f9771aaa041bf78638acdd0accb005dc41548e6
Giovanni <jacket.gav@gmail.com>
2023-12-03 16:40:47 +0100

    refactor(InfraStack): add Constant class, add Utils class, add constructs

---
+++ src/main/java/com/aerotrack/infrastructure/constructs/RefreshConstruct.java
@@ -0,0 +1,52 @@
+package com.aerotrack.infrastructure.constructs;
+
+import com.aerotrack.utils.Utils;
+import org.jetbrains.annotations.NotNull;
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.services.events.Rule;
+import software.amazon.awscdk.services.events.Schedule;
+import software.amazon.awscdk.services.events.targets.LambdaFunction;
+import software.amazon.awscdk.services.lambda.Code;
+import software.amazon.awscdk.services.lambda.Function;
+import software.amazon.awscdk.services.lambda.Runtime;
+import software.amazon.awscdk.services.s3.assets.AssetOptions;
+import software.amazon.jsii.JsiiObjectRef;
+import software.constructs.Construct;
+
+import java.util.List;
+
+import static com.aerotrack.model.Constant.REFRESH_EVENT_RATE_SECONDS;
+import static com.aerotrack.model.Constant.REFRESH_EVENT_RULE;
+import static com.aerotrack.model.Constant.REFRESH_LAMBDA;
+
+public class RefreshConstruct extends Construct {
+    protected RefreshConstruct(JsiiObjectRef objRef) {
+        super(objRef);
+    }
+
+    protected RefreshConstruct(InitializationMode initializationMode) {
+        super(initializationMode);
+    }
+
+    public RefreshConstruct(@NotNull Construct scope, @NotNull String id) {
+        super(scope, id);
+
+        // Define the Lambda function
+        Function refreshLambdaFunction = Function.Builder.create(this, Utils.getResourceName(REFRESH_LAMBDA))
+                .runtime(Runtime.JAVA_17)
+                .code(Code.fromAsset("src/main/java/com/aerotrack/software/", AssetOptions.builder()
+                        .bundling(Utils.getLambdaBuilderOptions()
+                                .command(Utils.getLambdaPackagingInstructions(REFRESH_LAMBDA))
+                                .build())
+                        .build()))
+                .handler("lambda.RefreshRequestHandler::handleRequest")
+                .build();
+
+        // Define the EventBridge rule that triggers the Lambda function
+        Rule refreshEventRule = Rule.Builder.create(this, Utils.getResourceName(REFRESH_EVENT_RULE))
+                .schedule(Schedule.rate(Duration.seconds(REFRESH_EVENT_RATE_SECONDS)))
+                .targets(List.of(new LambdaFunction(refreshLambdaFunction)))
+                .build();
+
+    }
+}
---
+++ src/main/java/com/aerotrack/utils/Utils.java
@@ -0,0 +1,45 @@
+package com.aerotrack.utils;
+
+import software.amazon.awscdk.BundlingOptions;
+import software.amazon.awscdk.DockerVolume;
+import software.amazon.awscdk.services.lambda.Runtime;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Collections.singletonList;
+import static software.amazon.awscdk.BundlingOutput.ARCHIVED;
+
+public class Utils {
+    public static BundlingOptions.Builder getLambdaBuilderOptions() {
+        return BundlingOptions.builder()
+                .image(Runtime.JAVA_17.getBundlingImage())
+                .volumes(singletonList(
+                        // Mount local .m2 repo to avoid download all the dependencies again inside the container
+                        DockerVolume.builder()
+                                .hostPath(System.getProperty("user.home") + "/.m2/")
+                                .containerPath("/root/.m2/")
+                                .build()
+                ))
+                .user("root")
+                .outputType(ARCHIVED);
+    }
+
+    public static String getResourceName(String resource) {
+        String devEnvironment = System.getenv("AEROTRACK_DEV");
+        if (devEnvironment == null || devEnvironment.isEmpty()) {
+            return resource;
+        }
+        return devEnvironment + "-" + resource;
+    }
+
+    public static List<String> getLambdaPackagingInstructions(String lambda) {
+        return Arrays.asList(
+                "/bin/sh",
+                "-c",
+                String.format("cd %s ", lambda) +
+                        "&& mvn clean install " +
+                        String.format("&& cp /asset-input/%s/target/%s-1.0-SNAPSHOT.jar /asset-output/", lambda, lambda)
+        );
+    }
+}
