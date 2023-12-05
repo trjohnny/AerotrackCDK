d36565a39b1a75093985bcbc854093fc930b4876
Giovanni <jacket.gav@gmail.com>
2023-12-05 22:20:09 +0100

    test: dependency debug

--- src/main/java/com/aerotrack/utils/Utils.java
+++ src/main/java/com/aerotrack/utils/Utils.java
@@ -44,7 +44,7 @@ public class Utils {
                 "/bin/sh",
                 "-c",
                 String.format("cd %s ", lambda) +
-                        "&& echo '<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd\"><servers><server><id>github</id><username>trjohnny</username><password>${GITHUB_TOKEN}</password></server></servers></settings>' > ~/.m2/settings.xml && mvn clean install " +
+                        "&& echo ${GITHUB_TOKEN} && echo '<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd\"><servers><server><id>github</id><username>trjohnny</username><password>${GITHUB_TOKEN}</password></server></servers></settings>' > ~/.m2/settings.xml && mvn clean install " +
                         String.format("&& cp /asset-input/%s/target/%s-1.0-SNAPSHOT.jar /asset-output/", lambda, lambda)
         );
     }
