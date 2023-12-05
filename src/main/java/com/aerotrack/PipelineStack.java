3c91a7486106fe1974375494def80a314a2da179
Giovanni <jacket.gav@gmail.com>
2023-12-05 21:30:04 +0100

    test: dependency debug

--- src/main/java/com/aerotrack/PipelineStack.java
+++ src/main/java/com/aerotrack/PipelineStack.java
@@ -1,14 +1,22 @@
 package com.aerotrack;
 
 import com.aerotrack.infrastructure.AppStage;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import software.amazon.awscdk.Environment;
 import software.amazon.awscdk.pipelines.*;
 import software.amazon.awscdk.StageProps;
+import software.amazon.awscdk.services.codebuild.BuildEnvironment;
+import software.amazon.awscdk.services.codebuild.BuildEnvironmentVariable;
+import software.amazon.awscdk.services.codebuild.BuildSpec;
 import software.constructs.Construct;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.Map;
 
 
 public class PipelineStack extends Stack {
@@ -16,8 +24,28 @@ public class PipelineStack extends Stack {
     public PipelineStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        Map<String, Object> buildSpecMap;
+
+        try {
+            String buildSpecJson = Files.readString(Path.of("buildspec.json"));
+            ObjectMapper objectMapper = new ObjectMapper();
+            buildSpecMap = objectMapper.readValue(buildSpecJson, Map.class);
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to read build specification", e);
+        }
+
+        CodeBuildOptions buildOptions = CodeBuildOptions.builder()
+                .partialBuildSpec(BuildSpec.fromObject(buildSpecMap))
+                .buildEnvironment(BuildEnvironment.builder()
+                        .environmentVariables(Map.of(
+                                "GITHUB_USERNAME", BuildEnvironmentVariable.builder().value("trjohnny").build(),
+                                "GITHUB_TOKEN", BuildEnvironmentVariable.builder().value("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").build()))
+                        .build())
+                .build();
+
         CodePipeline pipeline = CodePipeline.Builder.create(this, "AerotrackPipeline")
-            .pipelineName("AerotrackPipeline")
+                .pipelineName("AerotrackPipeline")
+                .codeBuildDefaults(buildOptions)
                 .crossAccountKeys(true)
                 .synth(ShellStep.Builder.create("Synth")
                 .input(CodePipelineSource.gitHub("trjohnny/AerotrackInfrastructure", "mainline"))
