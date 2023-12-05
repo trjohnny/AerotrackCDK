f51746f17430909b53c0a2505cf2918fde31e4ec
thomasrossimel <rossimelthomas@gmail.com>
2023-12-05 10:38:34 +0100

    refactor: data refresh logic

--- src/main/java/com/aerotrack/infrastructure/lambda/RefreshLambda/src/main/java/lambda/RefreshRequestHandler.java
+++ src/main/java/com/aerotrack/infrastructure/lambda/RefreshLambda/src/main/java/lambda/RefreshRequestHandler.java
@@ -1,6 +1,7 @@
 package lambda;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
 import com.amazonaws.services.s3.AmazonS3;
@@ -30,16 +31,16 @@ import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 import software.amazon.awssdk.enhanced.dynamodb.model.BatchWriteItemEnhancedRequest;
 import software.amazon.awssdk.enhanced.dynamodb.model.WriteBatch;
 
+import static utils.Constants.*;
+
 
 public class RefreshRequestHandler implements RequestHandler<ScheduledEvent, Void> {
 
-    public static final int DAY_PICK_WEIGHT_FACTOR = 30;
-    public static final String RYANAIR_API = "https://www.ryanair.com/api/booking/v4/it-it/availability?ADT=1&Origin=%s" +
-            "&Destination=%s&IncludeConnectingFlights=false&Disc=0&DateOut=%s&RoundTrip=false&ToUs=AGREED";
 
     public Void handleRequest(ScheduledEvent event, Context context) {
         // Retrieve JSON from S3
 
+        LambdaLogger logger = context.getLogger();
         AmazonS3 s3Client = AmazonS3ClientBuilder.standard().build();
         S3Object s3Object = s3Client.getObject(System.getenv("directionBucket"), "airports.json");
 
@@ -69,58 +70,58 @@ public class RefreshRequestHandler implements RequestHandler<ScheduledEvent, Voi
             URL url = URI.create(ryanairURL).toURL();
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("Cookie", "rid.sig=jyna6R42wntYgoTpqvxHMK7H+KyM6xLed+9I3KsvYZaVt7P36AL6zp9dGFPu5uVxaIiFpNXrszr+LfNCdY3IT3oCSYLeNv/ujtjsDqOzkY66AL3V6kH2vsK+au12X21HkZ4S8GaG8CoBmm/m0rLsOKYkxtw+U3+ejBaPc15jJjKXnc3owMBg82SNbqyKjVd6Z6qcsoE25p3RmlcaHuHC3GBf1yIGtlqeQun3Mj0vmSURVPQBjK65pge2zGBymoORV6PsmZ0Nabv73gS2VkhG+Eccz5iSIRPrZm5cloi/941TFo8oiXdyqvTMx/ozox2fkvaKB2vd/goSx543TxPdKoGKRLaDY3FoIepe6I46UFvXEZYszzugXHYRnp0lbIn/HPyvHH/iW/TXRrqsELQabKd1hH+Ut1ZgpfsKEtwDVyL7mVvi1qEOqHddSVKCN/439KxQqi9K03dQDm+knQaLRpzZL8EYqCeSaosMOeEhc2CAYWLW2D5jH5iTot0YiyaN3QJFE59H3MYDpGjoZfTfen83yVSpT8DBahOOr6Eibv62bKQXBxel5kIm75dZB26iUzmkBs1Iags291UJ8wpu/GtBD1rghlZoRJt9u3ASkAj3P85dBcV8MwGykVWJ4mCO; mkt=/gb/en/; .AspNetCore.Session=CfDJ8NJy2CjeBXdEiTlIkEo9jP1x6eP6igWNkoeL9uCto1qdz97HQLRlCAJWIhw97YY5uemEBnTrcLNnoB7lqKOnEJRzF%2F4yhGKN9A5COUteaQmZduO6o2whfYqdyD1Qd%2B%2BH6EXjFP4cd0DtvPDwguugs%2Bc684C2CSfw16lyvYFtSkvU; fr-correlation-id=b8d2a2fe-383b-44fa-8c4a-ba6cab8c994d; rid=034ed121-51ec-4cac-9196-97a50ee42d2c; RY_COOKIE_CONSENT=true; STORAGE_PREFERENCES={\"STRICTLY_NECESSARY\":true,\"PERFORMANCE\":false,\"FUNCTIONAL\":false,\"TARGETING\":false,\"SOCIAL_MEDIA\":false,\"PIXEL\":false,\"GANALYTICS\":true,\"__VERSION\":2}; myRyanairID=");
+            connection.setRequestProperty("Cookie", RYANAIR_COOKIE);
             connection.connect();
 
 
             // Read the response
-            InputStream responseStream = connection.getInputStream();
-            String jsonResponse = IOUtils.toString(responseStream, StandardCharsets.UTF_8);
+            try(InputStream responseStream = connection.getInputStream()) {
+                String jsonResponse = IOUtils.toString(responseStream, StandardCharsets.UTF_8);
 
-            // Parse the response JSON and extract flight details
-            JSONObject flightDetails = new JSONObject(jsonResponse);
-            JSONArray trips = flightDetails.optJSONArray("trips");
+                // Parse the response JSON and extract flight details
+                JSONObject flightDetails = new JSONObject(jsonResponse);
+                JSONArray trips = flightDetails.optJSONArray("trips");
 
-            if (trips != null && !trips.isEmpty()) {
-                JSONObject trip = trips.getJSONObject(0);
+                if (trips != null && !trips.isEmpty()) {
+                    JSONObject trip = trips.getJSONObject(0);
 
-                JSONArray dates = trip.optJSONArray("dates");
-                if (dates != null && !dates.isEmpty()) {
-                    JSONObject date = dates.getJSONObject(0);
+                    JSONArray dates = trip.optJSONArray("dates");
+                    if (dates != null && !dates.isEmpty()) {
+                        JSONObject date = dates.getJSONObject(0);
 
-                    JSONArray flights = date.optJSONArray("flights");
-                    if (flights != null && !flights.isEmpty()) {
-                        DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.create();
-                        DynamoDbTable<Flight> customerMappedTable = enhancedClient.table(System.getenv("flightTable"), TableSchema.fromBean(Flight.class));
+                        JSONArray flights = date.optJSONArray("flights");
+                        if (flights != null && !flights.isEmpty()) {
+                            DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.create();
+                            DynamoDbTable<Flight> customerMappedTable = enhancedClient.table(System.getenv("flightTable"), TableSchema.fromBean(Flight.class));
 
-                        WriteBatch.Builder<Flight> builder = WriteBatch.builder(Flight.class).mappedTableResource(customerMappedTable);
-                        for (int i = 0; i < flights.length(); i++) {
-                            JSONObject flight = flights.getJSONObject(i);
+                            WriteBatch.Builder<Flight> builder = WriteBatch.builder(Flight.class).mappedTableResource(customerMappedTable);
+                            for (int i = 0; i < flights.length(); i++) {
+                                JSONObject flight = flights.getJSONObject(i);
 
-                            String timeFrom = flight.getJSONArray("timeUTC").getString(0);
-                            String timeTo = flight.getJSONArray("timeUTC").getString(1);
-                            double price = flight.getJSONObject("regularFare").getJSONArray("fares").getJSONObject(0).getDouble("amount");
-                            String flightNumber = flight.getString("flightNumber");
+                                String timeFrom = flight.getJSONArray("timeUTC").getString(0);
+                                String timeTo = flight.getJSONArray("timeUTC").getString(1);
+                                double price = flight.getJSONObject("regularFare").getJSONArray("fares").getJSONObject(0).getDouble("amount");
+                                String flightNumber = flight.getString("flightNumber");
 
-                            Flight flightItem = new Flight(fromAirport + "-" + toAirport, timeFrom, timeTo, flightNumber, price);
-                            builder.addPutItem(flightItem);
+                                Flight flightItem = new Flight(fromAirport + "-" + toAirport, timeFrom, timeTo, flightNumber, price);
+                                builder.addPutItem(flightItem);
 
+                            }
+
+                            BatchWriteItemEnhancedRequest batchWriteItemEnhancedRequest = BatchWriteItemEnhancedRequest.builder()
+                                    .writeBatches(builder.build())
+                                    .build();
+
+                            enhancedClient.batchWriteItem(batchWriteItemEnhancedRequest);
                         }
-
-                        BatchWriteItemEnhancedRequest batchWriteItemEnhancedRequest = BatchWriteItemEnhancedRequest.builder()
-                                .writeBatches(builder.build())
-                                .build();
-
-                        enhancedClient.batchWriteItem(batchWriteItemEnhancedRequest);
                     }
                 }
             }
 
-            // Close resources
-            responseStream.close();
-
-        } catch (IOException e) {
-            System.err.println(e);
+        }
+        catch(IOException e)
+        {
+            logger.log("AN error occurred: " + e);
         }
 
         return null;
