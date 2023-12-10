b5310ebbb0428a2f38b0b114c35c11849890b20b
thomasrossimel <rossimelthomas@gmail.com>
2023-12-10 21:28:27 +0100

    feat: add airport refresh logic

---
+++ src/main/java/com/aerotrack/infrastructure/lambda/AirportsRefreshLambda/src/main/java/com/aerotrack/lambda/RefreshRequestHandler.java
@@ -0,0 +1,33 @@
+package com.aerotrack.lambda;
+
+import com.aerotrack.lambda.workflow.RefreshWorkflow;
+import com.aerotrack.utils.clients.s3.AerotrackS3Client;
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.ScheduledEvent;
+import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+
+import java.io.IOException;
+
+
+@Slf4j
+public class RefreshRequestHandler implements RequestHandler<ScheduledEvent, Void> {
+
+
+    public Void handleRequest(ScheduledEvent event, Context context) {
+
+
+        try {
+            RefreshWorkflow.create().refreshFlights();
+        }
+        catch (IOException | NullPointerException exc) {
+            log.error("An exception occurred: " + exc);
+        }
+
+        return null;
+    }
+}
+
+
