3be90885d1d86b9ba0a3980f3f73b16716800dea
Giovanni <jacket.gav@gmail.com>
2023-12-11 15:11:02 +0100

    fix: pipeline build

--- src/main/java/com/aerotrack/PipelineStack.java
+++ src/main/java/com/aerotrack/PipelineStack.java
@@ -14,6 +14,8 @@ import software.amazon.awscdk.StackProps;
 import java.util.Arrays;
 import java.util.Map;
 
+import static com.aerotrack.utils.Constant.GITHUB_USERNAME;
+
 
 public class PipelineStack extends Stack {
 
@@ -37,10 +39,28 @@ public class PipelineStack extends Stack {
                 .publishAssetsInParallel(false)
                 .crossAccountKeys(true)
                 .synth(ShellStep.Builder.create("Synth")
-                .input(CodePipelineSource.gitHub("trjohnny/AerotrackInfrastructure", "mainline"))
-                .commands(Arrays.asList("npm install -g aws-cdk", "cdk synth"))
-                .build())
-            .build();
+                        .input(CodePipelineSource.gitHub("trjohnny/AerotrackInfrastructure", "mainline"))
+                        .commands(Arrays.asList(
+                                "echo Creating Maven settings.xml",
+                                "echo '<settings xmlns=\"http://maven.apache.org/SETTINGS/1.0.0\" " +
+                                        "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
+                                        "xsi:schemaLocation=\"http://maven.apache.org/SETTINGS/1.0.0 " +
+                                        "http://maven.apache.org/xsd/settings-1.0.0.xsd\">" +
+                                            "<servers>" +
+                                                "<server>" +
+                                                    "<id>github</id>" +
+                                                    String.format("<username>%s</username>", GITHUB_USERNAME) +
+                                                    "<password>'${GITHUB_TOKEN}'</password>" +
+                                                "</server>" +
+                                            "</servers>" +
+                                        "</settings>' > ~/.m2/settings.xml",
+                                "npm install -g aws-cdk",
+                                "cdk synth"
+                        ))
+                        .build())
+                .build();
+
+
 
         StageDeployment alphaStage = pipeline.addStage(new AppStage(this, "Alpha", StageProps.builder()
                 .env(Environment.builder()
